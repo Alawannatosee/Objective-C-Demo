e0b696bbc547f338a7032c0db078dbba821f95c9
xuyingchao <1005024583@qq.com>
2020-11-03 15:32:15 +0800

    Initial Commit

---
+++ OC-test/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  OC-test
+//
+//  Created by xuyingchao on 2020/11/3.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
